052f9e5b54a6886b0ab36157e8ea2eb9369daf56
kornkritpawit <konsoo@hotmail.com>
2020-11-27 10:15:18 +0700

    finish layout pokemon list

--- src/views/PokeList.vue
+++ src/views/PokeList.vue
@@ -5,17 +5,21 @@
     </template>
     <template v-slot:section-content>
       <div class="poke-list">
-      <div class="pokemon" v-for="pokemon in pokemons" :key="pokemon.pokemonId">
-        <div>{{pokemon.pokemonId}}.</div>
-        <img :src="pokemon.image">
-        <div>{{ pokemon.name }}</div>
-        <div>hp = {{pokemon.hp}}</div>
-        <div>atk = {{pokemon.attack}}</div>
-        <div>defense = {{pokemon.defense}}</div>
+        <div
+          class="pokemon"
+          v-for="pokemon in pokemons"
+          :key="pokemon.pokemonId"
+        >
+          <div>{{ pokemon.pokemonId }}#</div>
+          <div><img :src="pokemon.image" /></div>
+          <div class="pokename">{{ pokemon.name }}</div>
+          <div class="pokeType">Type = {{ pokemon.primaryType}}</div>
+          <div class="pokeinfo">hp = {{ pokemon.hp }}</div>
+          <div class="pokeinfo">atk = {{ pokemon.attack }}</div>
+          <div class="pokeinfo">defense = {{ pokemon.defense }}</div>
+        </div>
       </div>
-    </div>
     </template>
-
   </Page>
 </template>
 
@@ -32,14 +36,28 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 img {
-  width:60px;
-  height:100%;
+  width: 60px;
+  height: 100%;
 }
 
+.pokemon > div {
+  margin-right: 1rem;
+}
 .pokemon {
+  margin: 1rem 0;
   display: flex;
-}
+  align-items: center;
 
+  .pokename {
+    width:10%;
+  }
+  .pokeType {
+    width: 18%
+  }
+  .pokeinfo {
+    width: 10%;
+  }
+}
 </style>
